a511063bdd8495a3bd9214d945497d578a6b964d
zzmOVO <381454126@qq.com>
2019-12-15 21:56:20 +0800

    handler

---
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView text;
+    public  static final int UPDATE_TEXT=1;
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case  UPDATE_TEXT:
+                    text.setText("nice to meet you");
+                    break;
+                    default:
+                        break;
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        Button changeText=(Button) findViewById(R.id.change);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.change:
+            new Thread(new Runnable() {
+                    @Override
+                    public void run () {
+                        Message message = new Message();
+                        message.what=UPDATE_TEXT;
+                        handler.sendMessage(message);
+                    }
+            }).start();
+            break;
+            default:
+                break;
+        }
+    }
+}
